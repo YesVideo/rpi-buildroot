bc09ca2e2a59a0aa7ee4a55c5904811affe5fe50
Andy Choi <chunkerchunker@gmail.com>
2015-12-28 12:22:15 -0800

    cleanup sb burn script

--- board/yesvideo/raspberrypi/burn_studio_sd.sh
+++ board/yesvideo/raspberrypi/burn_studio_sd.sh
@@ -6,6 +6,12 @@ NUM_FILE=$DIR/studio_number.txt
 HOSTFILE_TEMPLATE=$DIR/rootfs-overlay/etc/hostapd.conf.template
 HOSTFILE=$DIR/rootfs-overlay/etc/hostapd.conf
 
+if [ ! -b "/dev/sdb" ]
+then
+  echo "/dev/sdb is not mounted"
+  exit 1
+fi
+
 if [ ! -f $NUM_FILE ]; then
     echo "$NUM_FILE does not exist"
     exit 1
