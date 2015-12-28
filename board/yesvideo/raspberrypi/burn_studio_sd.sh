0a265e29c50d0154ea4bbc0ae9c972b1fc6f4110
Andy Choi <chunkerchunker@gmail.com>
2015-12-28 11:09:09 -0800

    sd card burn script

---
+++ board/yesvideo/raspberrypi/burn_studio_sd.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ROOT_DIR=`readlink -f $DIR/../../..`
+
+NUM_FILE=$DIR/studio_number.txt
+HOSTFILE_TEMPLATE=$DIR/rootfs-overlay/etc/hostapd.conf.template
+HOSTFILE=$DIR/rootfs-overlay/etc/hostapd.conf
+
+if [ ! -f $NUM_FILE ]; then
+    echo "$NUM_FILE does not exist"
+    exit 1
+fi
+
+N=`cat $NUM_FILE`
+N=`expr $N + 1`
+
+echo "Burning Studio #$N"
+
+sed -e "s/SSID/Studio_$N/g" $HOSTFILE_TEMPLATE > $HOSTFILE
+
+cd $ROOT_DIR
+make
+cd -
+
+umount /dev/sdb1
+umount /dev/sdb2
+
+sudo $ROOT_DIR/board/raspberrypi2/mksdcard /dev/sdb
+
+echo $N > $NUM_FILE
+
+echo -e '\a'
